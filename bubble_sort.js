f176c06873a1163244d4a70ca8d65de707176717
AdityaGaikar91 <gaikaditya795@gmail.com>
2025-08-28 17:20:18 +0530

    Updated

--- bubble_sort.js
+++ bubble_sort.js
@@ -1,20 +1,29 @@
 const arr = [6, 0, 3, 5, 7];
 let n = arr.length;
 let temp;
-let swapped = false;
 
-for (let i = 0; i<n; i++){
+function bubbleSort(arr, n) {
+  for (let i = 0; i < n; i++) {
+    let swapped = false;
 
-    for (let j = 0; j< n-i-1; j++){
-        if(arr[j] > arr[j+1]){
-            // swap
-            temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
-            swapped = true;
-        }
+    for (let j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        // swap
+        temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        swapped = true;
+      }
     }
 
-    if(swapped )
+    if (swapped == false) {
+      break;
+    }
+  }
 }
 
+bubbleSort(arr, n);
+console.log(arr);
+
+
+// Time Compex
--- min_max_array.js
+++ min_max_array.js
@@ -15,3 +15,23 @@ for (let i =0; i<a.length; i++){
 
 console.log("Maximum no. is", max);
 console.log("Minimum no. is", min);
+
+
+// Two Sum Problem
+
+// /**
+//  * @param {number[]} nums
+//  * @param {number} target
+//  * @return {number[]}
+//  */
+// var twoSum = function(nums, target) {
+//     let ans =[];
+//     for(let i = 0; i<nums.length-1; i++){
+//         for(let j=i+1; j<nums.length;j++){
+//             if(nums[i]+nums[j]== target){
+//                 ans.push(i, j);// ans.push(j, i);
+//                 return ans;
+//             }
+//         }
+//     }
+// };
